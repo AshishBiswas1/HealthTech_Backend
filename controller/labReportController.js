c9c07584adc3bbf3d58a470aab57fa5b0619bc8f
AshishBiswas1 <biswasashish655@gmail.com>
2025-08-30 19:04:46 +0530

    Second commit

--- controller/labReportController.js
+++ controller/labReportController.js
@@ -20,8 +20,6 @@ exports.addLabReport = catchAsync(async (req, res, next) => {
     });
 });
 
-console.log("Ok");
-
 exports.getAllLabReports = catchAsync(async (req, res, next) => {
     const labreports = await labReport.find();
 
